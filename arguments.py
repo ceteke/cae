0bbfae005b271a5661c2ac4c8d2423734ba24598
Cem Eteke <cemeteke@gmail.com>
2017-10-06 02:58:42 +0300

    requirement fix

--- arguments.py
+++ arguments.py
@@ -6,14 +6,14 @@ def get_parser():
     parser.add_argument("-e", "--epochs", help="Number of epochs", required=True, dest='num_epochs', type=int)
     parser.add_argument("-i", "--info", help="Info steps", required=True, dest='info_step', type=int)
     parser.add_argument("-b", "--batch", help="Batch size", required=True, dest='batch_size', type=int)
-    parser.add_argument("-l", "--layers", help="Layer string. See README.md", required=True, dest='layer_str', type=str,
+    parser.add_argument("-l", "--layers", help="Layer string. See README.md", required=False, dest='layer_str', type=str,
                         default='(64)3c-4p-(64)3c-3p-(128)3c-(128)3c-2p-(256)3c-(256)3c-(256)3c-(512)3c-(512)3c-(512)3c-2p')
     parser.add_argument("-lr", "--learning_rate", help="Learning rate", required=True, dest='learning_rate', type=float)
 
 
     parser.add_argument("-lm", "--lambda_M", help="Middle loss coefficient", required=False, dest='lambda_M',
                         type=float, default=0.2)
-    parser.add_argument("-lrec", "--lambda_rec", help="Reconstruction loss coefficient", required=True, dest='lambda_rec',
+    parser.add_argument("-lrec", "--lambda_rec", help="Reconstruction loss coefficient", required=False, dest='lambda_rec',
                         type=float, default=1.0)
     parser.add_argument("-tb", "--tensorboard", help="ID of tensorboard", required=False, dest='tensorboard_id',
                         type=int)
