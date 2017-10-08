f81b958179868b86a30dd972157c75a585cc4dbd
Cem Eteke <cemeteke@gmail.com>
2017-10-08 05:34:27 +0300

    emb output dir

--- arguments.py
+++ arguments.py
@@ -25,6 +25,7 @@ def get_parser():
 def get_emb_parser():
     parser = argparse.ArgumentParser(description='SWWAE Embedding')
     parser.add_argument("-b", "--batch", help="Batch size", required=True, dest='batch_size', type=int)
+    parser.add_argument("-o", "--output", help="Output directory", required=True, dest='out_dir', type=str)
     parser.add_argument("-l", "--layers", help="Layer string. See README.md", required=False, dest='layer_str',
                         type=str,
                         default='(64)3c-4p-(64)3c-3p-(128)3c-(128)3c-2p-(256)3c-(256)3c-(256)3c-(512)3c-(512)3c-(512)3c-2p')
--- form_embedding.py
+++ form_embedding.py
@@ -19,7 +19,7 @@ sess = tf.Session()
 save_sess = tf.Session()
 swwae = SWWAE(sess,[32,32,3],'embedding',layers)
 
-swwae.restore('output/swwae')
+swwae.restore(os.path.join(parsed.out_dir, 'swwae'))
 
 X_test, _ = dataset.get_batches(parsed.batch_size, train=False)
 test_steps = len(X_test)
