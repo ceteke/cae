4ac8460f59b41a052b83aaa7a4845d86c7b7bf25
Cem Eteke <cemeteke@gmail.com>
2017-10-08 03:13:41 +0300

    silly fix

--- form_embedding.py
+++ form_embedding.py
@@ -28,7 +28,7 @@ for test_step in range(test_steps):
     if test_step == 0:
         embedding_matrix = representation
     else:
-        np.concatenate((embedding_matrix, X_test_step))
+        np.concatenate((embedding_matrix, representation))
 
 print(embedding_matrix.shape)
 
