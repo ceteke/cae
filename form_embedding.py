77da546977b31a6502fd05735a344d2e09cf0e01
Cem Eteke <cemeteke@gmail.com>
2017-10-08 03:12:58 +0300

    embedding matrix formation

--- form_embedding.py
+++ form_embedding.py
@@ -3,6 +3,7 @@ from model import SWWAE
 import tensorflow as tf
 from utils import parse_layers
 from arguments import get_emb_parser
+import numpy as np
 
 parser = get_emb_parser()
 parsed = parser.parse_args()
@@ -22,6 +23,12 @@ test_steps = len(X_test)
 
 for test_step in range(test_steps):
     X_test_step = X_test[test_step]
-
     representation = swwae.get_representation(input=X_test_step)
-    print(representation.shape)
+
+    if test_step == 0:
+        embedding_matrix = representation
+    else:
+        np.concatenate((embedding_matrix, X_test_step))
+
+print(embedding_matrix.shape)
+
