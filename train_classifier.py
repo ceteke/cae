eacb8dc6651d888e74f085e0d01d667677675a8e
Cem Eteke <cemeteke@gmail.com>
2017-10-11 01:26:13 +0300

    svm classifier train

--- train_classifier.py
+++ train_classifier.py
@@ -1,113 +1,57 @@
-from datapy.data.datasets import CIFAR10Dataset
+from datapy.data.datasets import CIFAR10Dataset, MNISTDataset
 from model import SWWAE
 import tensorflow as tf
 from utils import parse_layers
-from arguments import get_class_parser
-from utils import save_loss, clear_loss, accuracy
-import time
+from arguments import get_emb_parser
+import os
+import numpy as np
+from sklearn import svm, metrics
 
-def main():
-    clear_loss()
-    parser = get_class_parser()
-    parsed = parser.parse_args()
+parser = get_emb_parser()
+parsed = parser.parse_args()
 
-    assert ((parsed.output_dir is None and parsed.save_step is None) or
-            (parsed.output_dir is not None and parsed.save_step is not None)), "Save step and output directory must be " \
-                                                                               "null at the same time or not null at the same time"
-
+if parsed.dataset == 'cifar10':
     dataset = CIFAR10Dataset()
     dataset.process()
+    img_size = [32,32,3]
+else:
+    dataset = MNISTDataset()
+    dataset.process()
+    img_size = [28,28,1]
 
-    encoder_train = True if parsed.encoder_train==1 else False
+if parsed.fc_layers is not None:
+    fc_layers = [int(x) for x in parsed.fc_layers.split('-')]
+else:
+    fc_layers = []
 
-    layers = parse_layers(parsed.layer_str)
-    fc_layers = parsed.fc_layers.split('-')
+layers = parse_layers(parsed.layer_str)
 
-    sess = tf.Session()
-    swwae = SWWAE(sess,[32,32,3],'classification',layers,fc_layers=fc_layers,learning_rate=parsed.learning_rate,
-                  tensorboard_id=parsed.tensorboard_id, num_classes=10, encoder_train=encoder_train)
-    swwae.restore(parsed.load_dir)
+sess = tf.Session()
+save_sess = tf.Session()
+swwae = SWWAE(sess,img_size,'embedding',layers,fc_layers)
 
-    X, y = dataset.get_batches(parsed.batch_size)
-    X_test, y_test = dataset.get_batches(parsed.batch_size, train=False)
-    print("Started training.\nTrain steps: {}".format(len(X)))
+swwae.restore(os.path.join(parsed.out_dir))
 
-    for e in range(parsed.num_epochs):
-        total_loss = 0.0
-        total_accuracy = 0.0
-        epoch_loss = 0.0
-        epoch_acc = 0.0
-        train_steps = len(X)
-        start = time.time()
-        for step in range(train_steps):
-            X_step = X[step]
-            y_step = y[step]
+X_train, y_train = dataset.get_batches(parsed.batch_size,shuffle=False)
+train_steps = len(X_train)
 
-            loss, acc, global_step = swwae.train(X_step,y_step)
+print("Forming training embedding matrix")
 
-            total_loss += loss
-            total_accuracy += acc
-            epoch_loss += loss
-            epoch_acc += acc
+for train_step in range(train_steps):
+    X_train_step = X_train[train_step]
+    representation = swwae.get_representation(input=X_train_step)
 
-            if (step + 1) % parsed.info_step == 0:
-                avg_loss = total_loss / parsed.info_step
-                avg_acc = total_accuracy / parsed.info_step
-                save_loss(avg_loss)
+    if train_step == 0:
+        embedding_matrix = representation
+    else:
+        embedding_matrix = np.concatenate((embedding_matrix, representation))
 
-                total_loss_test = 0.0
-                total_acc_test = 0.0
-                test_steps = len(X_test)
+print(embedding_matrix.shape)
 
-                for test_step in range(test_steps):
-                    X_test_step = X_test[test_step]
-                    y_test_step = y_test[test_step]
+clf = svm.SVC(decision_function_shape='ovo')
+clf.fit(embedding_matrix, dataset.training_labels)
+y_pred = clf.predict(dataset.test_data)
 
-                    loss, acc = swwae.eval(input=X_test_step, labels=y_test_step)
+acc = metrics.accuracy_score(dataset.test_labels, y_pred)
 
-                    total_loss_test += loss
-                    total_acc_test += acc
-
-                #print("Test average loss: {}, average acc: {}".format(total_loss_test / test_steps, total_acc_test / test_steps))
-
-                #print("Train epoch {}:\n\tstep {}\n\tAvg Loss: {} Avg accuracy {}".format(e + 1, step + 1, avg_loss, avg_acc),
-                      #flush=True)
-
-                total_loss = 0.0
-                total_accuracy = 0.0
-                end = time.time()
-                hours, rem = divmod(end - start, 3600)
-                minutes, seconds = divmod(rem, 60)
-                #print("Elapsed: {:0>2}:{:0>2}:{:0>2}".format(int(hours), int(minutes), int(seconds)), flush=True)
-                start = time.time()
-
-            if parsed.save_step is not None:
-                if (global_step + 1) % parsed.save_step == 0:
-                    swwae.save(path=parsed.output_dir)
-
-        print("Train epoch {}: avg. loss: {}, avg. acc: {}".format(e + 1, epoch_loss / train_steps, epoch_acc / train_steps), flush=True)
-        X, y = dataset.get_batches(parsed.batch_size)
-
-    if parsed.output_dir is not None:
-        swwae.save(path=parsed.output_dir)
-
-    print("Starting test..")
-
-    total_loss = 0.0
-    total_acc = 0.0
-    test_steps = len(X_test)
-
-    for test_step in range(test_steps):
-        X_test_step = X_test[test_step]
-        y_test_step = y_test[test_step]
-
-        loss, acc = swwae.eval(input=X_test_step,labels=y_test_step)
-
-        total_loss += loss
-        total_acc += acc
-
-    print("Final Test average loss: {}, average acc: {}".format(total_loss/test_steps, total_acc/test_steps))
-
-
-if __name__ == "__main__":
-   main()
+print("Test acc:{}".format(acc))
