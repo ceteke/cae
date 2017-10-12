c46ca76de0b6fd704bd218e9608d0772216d6385
Cem Eteke <cemeteke@gmail.com>
2017-10-13 00:38:26 +0300

    CemNet :))

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tf_utils import max_unpool, variable_on_cpu, variable_with_weight_decay, max_pool_with_argmax
 
 class SWWAE:
     def __init__(self, sess, image_shape, mode, layers, fc_ae_layers=None, fc_layers=None, learning_rate=None, lambda_rec=None,
@@ -27,12 +26,9 @@ class SWWAE:
                                     dtype=self.dtype, name='input_batch')
         self.dropout_rate = tf.placeholder(shape=(), dtype=tf.float32)
         self.global_step = tf.Variable(0, trainable=False)
-        if self.mode == 'classification':
-            self.labels = tf.placeholder(shape=[None,], dtype=tf.int64, name='labels')
 
     def encoder_forward(self):
         encoder_whats = []
-        encoder_wheres = []
         encoder_what = self.input
 
         for i, layer in enumerate(self.layers):
@@ -42,18 +38,14 @@ class SWWAE:
 
             # pooln
             if layer.pool_size is not None:
-                encoder_what, encoder_where = max_pool_with_argmax(encoder_what, layer.pool_size, layer.pool_size)
-                encoder_wheres.append(encoder_where)
-
-            else:
-                encoder_wheres.append(None)
+                encoder_what = tf.layers.max_pooling2d(encoder_what, layer.pool_size, layer.pool_size,
+                                                                      padding='same')
 
             encoder_whats.append(encoder_what)
 
         self.encoder_whats = encoder_whats
         pool_shape = encoder_whats[-1].get_shape()
         self.flatten = tf.reshape(encoder_whats[-1], [-1, (pool_shape[1] * pool_shape[2] * pool_shape[3]).value])
-        self.encoder_wheres = encoder_wheres
 
         if len(self.fc_ae_layers) == 0:
             self.representation = self.flatten
@@ -93,19 +85,40 @@ class SWWAE:
         decoder_whats = []
         for i in range(len(self.layers)-1, -1, -1):
             layer = self.layers[i]
-            #unpooln
-            if self.encoder_wheres[i] is not None:
-                decoder_what = max_unpool(decoder_what, self.encoder_wheres[i], layer.pool_size)
 
             with tf.variable_scope('deconv{}'.format(i+1)):
-                if i == 0: # Does not use non-linearity at the last layer
-                    shape = self.image_shape[-1]
-                    decoder_what = tf.layers.conv2d_transpose(decoder_what, shape, layer.filter_size, padding='same')
+                if layer.pool_size is None:
+                    if i == 0:  # Does not use non-linearity at the last layer
+                        shape = self.image_shape[-1]
+                        activation = None
+                    else:
+                        shape = self.layers[i - 1].channel_size
+                        activation = tf.nn.relu
+
+                    decoder_what = tf.layers.conv2d(decoder_what, shape, layer.filter_size, activation=activation,
+                                                        padding='same',)
                 else:
-                    shape = self.layers[i - 1].channel_size
-                    print(decoder_what)
-                    decoder_what = tf.layers.conv2d_transpose(decoder_what, shape, layer.filter_size,
-                                               activation=tf.nn.relu, padding='same')
+                    if i == 0: # Does not use non-linearity at the last layer
+                        output_shape = self.input.get_shape()
+                        filter_size = [layer.filter_size, layer.filter_size, self.image_shape[-1], layer.channel_size]
+                        bias_size = self.image_shape[-1]
+                    else:
+                        up = self.encoder_whats[i-1]
+                        output_shape = up.get_shape()
+                        filter_size = [layer.filter_size, layer.filter_size, self.layers[i-1].channel_size, layer.channel_size]
+                        bias_size = self.layers[i-1].channel_size
+
+                    filter = tf.get_variable('filter', shape=filter_size, dtype=self.dtype,
+                                             initializer=tf.glorot_uniform_initializer(dtype=self.dtype))
+                    bias = tf.get_variable('bias', shape=bias_size, dtype=self.dtype,
+                                           initializer=tf.constant_initializer(0.0, dtype=self.dtype))
+
+                    decoder_what = tf.nn.conv2d_transpose(decoder_what, filter, output_shape,
+                                                          strides=[1,layer.pool_size,layer.pool_size,1])
+                    decoder_what = tf.nn.bias_add(decoder_what, bias)
+
+                    if i != 0:
+                        decoder_what = tf.nn.relu(decoder_what)
 
                 decoder_whats.append(decoder_what)
 
@@ -114,41 +127,8 @@ class SWWAE:
                 tf.add_to_collection('losses', middle_loss)
 
         self.decoder_what = decoder_what
-
-    def fully_connected_forward(self):
-        representation = self.representation
-        dim = self.representation.get_shape()[1].value
-        locals = []
-
-        for i, units in enumerate(self.fc_layers):
-            with tf.variable_scope('fc{}'.format(i+1)):
-                if i == 0:
-                    inp_dim = dim
-                else:
-                    inp_dim = self.fc_layers[i-1]
-
-                weights = variable_with_weight_decay('weights', shape=[inp_dim, units],
-                                                      stddev=0.04, wd=0.001, dtype=self.dtype, trainable=True)
-                biases = variable_on_cpu('biases', [units], tf.constant_initializer(0.1), dtype=self.dtype,
-                                         trainable=True)
-                if i == 0:
-                    rep_drop = tf.nn.dropout(representation, 0.5)
-                    local = tf.nn.relu(tf.matmul(rep_drop, weights) + biases, name='local')
-                else:
-                    local = tf.nn.relu(tf.matmul(locals[i-1], weights) + biases, name='local')
-
-                locals.append(local)
-
-        with tf.variable_scope('softmax_linear'):
-            non_linear = locals[-1]
-            weights = variable_with_weight_decay('weights', shape=[self.fc_layers[-1], self.num_classes],
-                                                 stddev=1 / 192.0, wd=0.0, dtype=self.dtype, trainable=True)
-            biases = variable_on_cpu('biases', [self.num_classes], tf.constant_initializer(0.0), dtype=self.dtype,
-                                     trainable=True)
-
-            output = tf.add(tf.matmul(non_linear, weights), biases, name='output')
-
-        return output
+        print(self.encoder_whats)
+        print(decoder_whats)
 
 
     def ae_loss(self):
@@ -157,16 +137,6 @@ class SWWAE:
         ae_loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
         tf.summary.scalar('loss', ae_loss)
         return ae_loss
-
-    def softmax_loss(self, fc_out):
-        labels = tf.cast(self.labels, tf.int64)
-        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-            labels=labels, logits=fc_out, name='cross_entropy_per_example')
-        cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
-        tf.add_to_collection('losses', cross_entropy_mean)
-        s_loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
-        tf.summary.scalar('classification loss', s_loss)
-        return s_loss
 
     def init_optimizer(self, loss):
         optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
@@ -183,17 +153,6 @@ class SWWAE:
             self.init_optimizer(self.ae_loss)
             tf.summary.image('whatwhere/stacked', tf.concat((self.input, self.decoder_what), axis=2))
 
-        elif self.mode == 'classification':
-            print("Forming fully connected")
-            fc_out = self.fully_connected_forward()
-            self.s_loss = self.softmax_loss(fc_out)
-            print("Forming classification optimizier with learning rate{}".format(self.learning_rate))
-            self.predictions = tf.argmax(fc_out, axis=1)
-            correct_pred = tf.equal(self.labels, self.predictions)
-            self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-            tf.summary.scalar('batch accuracy', self.accuracy)
-            self.init_optimizer(self.s_loss)
-
         self.merged = tf.summary.merge_all()
         self.train_writer = tf.summary.FileWriter('tensorboard/{}/train'.format(self.tensorboard_id), self.sess.graph)
         self.test_writer = tf.summary.FileWriter('tensorboard/{}/test'.format(self.tensorboard_id), self.sess.graph)
@@ -206,23 +165,12 @@ class SWWAE:
                                                        feed_dict={self.input: input, self.dropout_rate: 0.5})
             self.train_writer.add_summary(tb_merge, global_step)
             return batch_loss, global_step
-        elif self.mode == 'classification':
-            _, batch_loss, predictions, accuracy, global_step, tb_merge = self.sess.run([self.opt_op, self.s_loss, self.predictions, self.accuracy,
-                                                                                        self.global_step, self.merged],
-                                                                                        feed_dict={self.input: input, self.labels:labels, self.dropout_rate:0.5})
-            self.train_writer.add_summary(tb_merge, global_step)
-            return batch_loss, accuracy, global_step
 
     def eval(self, input, labels=None):
         if self.mode == 'autoencode':
             loss, tb_merge, global_step = self.sess.run([self.ae_loss, self.merged, self.global_step], feed_dict={self.input:input, self.dropout_rate:0.0})
             self.test_writer.add_summary(tb_merge, global_step)
             return loss
-        elif self.mode == 'classification':
-            loss, accuracy, tb_merge, global_step = self.sess.run([self.s_loss, self.accuracy, self.merged, self.global_step],
-                                                                  feed_dict={self.input:input, self.labels:labels, self.dropout_rate:0.0})
-            self.test_writer.add_summary(tb_merge, global_step)
-            return loss, accuracy
 
     def get_representation(self, input):
         return self.sess.run(self.representation, feed_dict={self.input:input, self.dropout_rate:0.0})
@@ -237,11 +185,5 @@ class SWWAE:
         print('model saved at {}'.format(save_path), flush=True)
 
     def restore(self, path):
-        if self.mode == 'classification':
-            var_list = []
-            for i, _ in enumerate(self.layers):
-                var_list += tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='conv{}'.format(i+1))
-            saver = tf.train.Saver(var_list=var_list)
-        else:
-            saver = tf.train.Saver()
-        saver.restore(self.sess, save_path=path)
+       saver = tf.train.Saver()
+       saver.restore(self.sess, save_path=path)
