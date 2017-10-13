7bff5753acc07294b7c55904f1a33154bc15d1c4
Cem Eteke <cemeteke@gmail.com>
2017-10-14 01:55:17 +0300

    preprocessing

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -60,8 +60,8 @@ def main():
         samplewise_center=False,  # set each sample mean to 0
         featurewise_std_normalization=False,  # divide inputs by std of the dataset
         samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        zca_whitening=True,  # apply ZCA whitening
+        rotation_range=10.0,  # randomly rotate images in the range (degrees, 0 to 180)
         width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
         height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
         horizontal_flip=True,  # randomly flip images
