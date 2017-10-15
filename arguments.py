3477b49f76e3131a9bfd409fb875e439b37651a3
Cem Eteke <cemeteke@gmail.com>
2017-10-15 21:39:20 +0300

    int fix

--- arguments.py
+++ arguments.py
@@ -43,7 +43,7 @@ def get_emb_parser():
     parser.add_argument("-s", "--save", help="Save embedding path", required=False, dest='save_path',
                         type=str, default='embedding')
     parser.add_argument("-fc", "--full_connected", help="Fully connected layers", required=False, dest='fc_layers',
-                        type=str)
+                        type=int)
     parser.add_argument("-ds", "--dataset", help="Which dataset", required=True, dest='dataset',
                         type=str)
 
