1aa9dc90c9a5272b4cbf71a2a595f0ab76c4e465
Cem Eteke <cemeteke@gmail.com>
2017-10-17 13:36:08 +0300

    loss fix

--- model.py
+++ model.py
@@ -173,15 +173,9 @@ class SWWAE:
 
         total_loss = tf.add_n(losses, name='total_loss')
 
-        loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
-        loss_averages_op = loss_averages.apply(losses + [total_loss])
-
         for l in losses + [total_loss]:
             loss_name = l.op.name
             tf.summary.scalar(loss_name, l)
-
-        with tf.control_dependencies([loss_averages_op]):
-            total_loss = tf.identity(total_loss)
 
         return total_loss
 
