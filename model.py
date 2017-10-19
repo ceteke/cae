batch normalization fix 2

model.py
@@ -63,8 +63,7 @@ class SWWAE:
             self.representation = self.flatten
         else:
             with tf.name_scope('encoder_fc'):
-                encoder_fc = tf.layers.batch_normalization(self.flatten, training=self.train_time)
-                encoder_fc = tf.layers.dense(encoder_fc,self.rep_size, activation=tf.nn.relu, kernel_initializer=self.kernel_initializer,
+                encoder_fc = tf.layers.dense(self.flatten,self.rep_size, activation=tf.nn.relu, kernel_initializer=self.kernel_initializer,
                                              kernel_regularizer=self.regulazier, bias_initializer=self.bias_initializer)
                 tf.summary.histogram('representation', encoder_fc)
 
@@ -76,7 +75,7 @@ class SWWAE:
                 kl_divergence = tf.multiply(p, (tf.log(p) - tf.log(p_hat + 1e-3))) + tf.multiply(tf.subtract(one, p),
                                                                                           (tf.log(tf.subtract(one, p)) - tf.log(tf.subtract(one, p_hat) + 1e-3)))
                 kl_divergence = tf.multiply(self.beta, tf.reduce_sum(kl_divergence), name='sparsity')
-                tf.add_to_collection('losses', kl_divergence)
+                # tf.add_to_collection('losses', kl_divergence)
 
             self.representation = encoder_fc
 
@@ -85,8 +84,7 @@ class SWWAE:
             decoder_what = self.encoder_what
         else:
             with tf.name_scope('decoder_fc'):
-                decoder_what = tf.layers.batch_normalization(self.representation, training=self.train_time)
-                decoder_what = tf.layers.dense(decoder_what,self.flatten.get_shape()[1].value,kernel_initializer=self.kernel_initializer,
+                decoder_what = tf.layers.dense(self.representation,self.flatten.get_shape()[1].value,kernel_initializer=self.kernel_initializer,
                                              kernel_regularizer=self.regulazier, bias_initializer=self.bias_initializer, activation=tf.nn.relu)
 
                 pool_shape = self.encoder_what.get_shape()
@@ -94,7 +92,6 @@ class SWWAE:
 
         for i in range(len(self.layers)-1, -1, -1):
             layer = self.layers[i]
-            decoder_what = tf.layers.batch_normalization(decoder_what, training=self.train_time)
             #unpooln
             if self.encoder_wheres[i] is not None:
                 decoder_what = max_unpool(decoder_what, self.encoder_wheres[i], layer.pool_size)